b5fdcc576563816e96d4ca07652b08d2c3ca3c58
FAZELI SHAHROUDI Sepehr (INTERN) <sepehr.fazelishahroudi.intern@3ds.com>
2024-11-01 14:36:53 +0100

    done

---
+++ practice/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+const aConst uint8 = 64
+
+func main() {
+
+	var aString string = "This is Go!"
+	fmt.Println(aString)
+	fmt.Printf("The variable's type is %T\n", aString)
+
+	var anInterger int = 42
+	fmt.Println(anInterger)
+
+	var defaultInt int
+	fmt.Println(defaultInt)
+
+	var anotherString = "This is another string"
+	fmt.Println(anotherString)
+	fmt.Printf("The variable's type is %T\n", anotherString)
+
+	var anotherInt = 53
+	fmt.Println(anotherInt)
+	fmt.Printf("The variable's type is %T\n", anotherInt)
+
+	myString := "This is also a string"
+	fmt.Println(myString)
+	fmt.Printf("The variable's type is %T\n", myString)
+
+	fmt.Println(aConst)
+	fmt.Printf("The variable's type is %T\n", aConst)
+}
