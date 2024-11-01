5ebc1167c74c6b53c5aa578aeaff562d97b4c3bf
FAZELI SHAHROUDI Sepehr (INTERN) <sepehr.fazelishahroudi.intern@3ds.com>
2024-11-01 15:22:29 +0100

    done

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integers sum:", intSum)
 
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Floats sum:", floatSum)
+
+	floatSum = math.Round(floatSum*100) / 100
+	fmt.Println("The sum is now", floatSum)
+
+	circleRadius := 15.5
+	circumference := circleRadius * 2 * math.Pi
+	fmt.Printf("circumference: %.2f\n", circumference)
 }
